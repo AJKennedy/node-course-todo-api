20a339b18160e67a0d192393df4edd509c328fe8
adam.kennedy <akennedy@nextit.com>
2018-04-03 15:21:18 -0700

    Add get todos by id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser')
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
+const {ObjectId} = require('mongodb');
 
 var app = express();
 
@@ -30,6 +31,29 @@ app.get('/todos', (req, res) => {
   });
 });
 
+//Get /todos/12345
+app.get('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+
+  if(!ObjectId.isValid(id))
+  {
+      return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(todo)
+    {
+      return res.send({todo});
+    }
+
+    res.status(404).send();
+
+  }, (err) => {
+    res.status(400).send();
+  });
+
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
