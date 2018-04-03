6278d439aa31b3fe6c88c70efc98ba14ef21fed1
adam.kennedy <akennedy@nextit.com>
2018-04-03 16:08:12 -0700

    Addition of tests. Slight formatting update

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.get('/todos/:id', (req, res) => {
       return res.send({todo});
     }
 
-    res.status(404).send();
+  res.status(404).send();
 
   }, (err) => {
     res.status(400).send();
