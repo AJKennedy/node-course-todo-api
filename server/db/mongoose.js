3863cf9970abf50953d2ed99a794bfb4bcd87fee
adam.kennedy <akennedy@nextit.com>
2018-04-04 10:31:30 -0700

    mlab url update

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://appUser:TestPassword@ds149268.mlab.com:49268/todoapp'
+  mlab: 'mongodb://appUser:TestPassword@ds233739.mlab.com:33739/todo-app-api'
 };
 
 mongoose.connect( db.mlab);
